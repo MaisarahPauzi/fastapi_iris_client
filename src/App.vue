b3fe9366abcf9042a18541c0f6894de6f0f3a419
MaisarahPauzi <maisarah.mohamed.pauzi@gmail.com>
2020-11-19 01:33:20 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    KNN Iris Species Predictor
+    </v-app-bar>
+      
+    <v-main>
+     <v-card
+         class="12sx"
+         max-width="100%"
+         
+       >
+       <div align="center">
+       <v-img src="https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Machine+Learning+R/iris-machinelearning.png" width="50%">
+       </v-img>
+
+       </div>
+       
+         <v-card-text>
+           <h3>Select values below</h3>
+         </v-card-text>
+        <div class="sliders-div">
+          <v-slider
+            v-model="ex1.val"
+            :label="ex1.label"
+            :thumb-color="ex1.color"
+            thumb-label="always"
+            :max="8"
+            step="0.1"
+          ></v-slider>
+          <v-slider
+            v-model="ex2.val"
+            :label="ex2.label"
+            :thumb-color="ex2.color"
+            thumb-label="always"
+            :max="8"
+            step="0.1"
+          ></v-slider>
+          <v-slider
+            v-model="ex3.val"
+            :label="ex3.label"
+            :thumb-color="ex3.color"
+            thumb-label="always"
+            :max="8"
+            step="0.1"
+          ></v-slider>
+          <v-slider
+            v-model="ex4.val"
+            :label="ex4.label"
+            :thumb-color="ex4.color"
+            thumb-label="always"
+            :max="3"
+            step="0.1"
+          ></v-slider>
+
+        </div>
+        <div align="center">
+              <v-btn color="primary" @click="btnclick">SUBMIT</v-btn>
+          
+        </div>
+       </v-card>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data () {
+      return {
+        ex1: { label: 'Sepal Length', val: 0, color: 'red' },
+        ex2: { label: 'Sepal Width', val: 0, color: 'red' },
+        ex3: { label: 'Petal Length', val: 0, color: 'red' },
+        ex4: { label: 'Petal Width', val: 0, color: 'red' },
+      }
+    },
+    methods:{
+    btnclick(){
+      let jsonData = {
+        "sepal_length": this.ex1.val,
+        "sepal_width": this.ex2.val,
+        "petal_length": this.ex3.val,
+        "petal_width": this.ex4.val
+      }
+      fetch('http://localhost:8000/predict', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.$swal.fire('Results',
+                      `Species Predicted: ${data.prediction}  (Probability: ${data.probability})`,
+                      'info');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+      
+      
+    }
   }
-}
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.sliders-div {
+  padding: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import vuetify from './plugins/vuetify';
+import VueSweetalert2 from 'vue-sweetalert2';
+ 
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+ 
+Vue.use(VueSweetalert2);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
